745e3b949ba0498bc15fb91e5306763bfc1af732
Mark Benders <bendersmark573@gmail.com>
2026-03-03 10:11:38 -0500

    started basic layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, TouchableHighlight, Image } from 'react-native'
 import React from 'react'
+
+let deviceHeight = Dimensions.get('window').height;
+let deviceWidth = Dimensions.get('window').width;
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Habit Tracker</Text> 
+          <TouchableHighlight onPress={() => alert('Logo pressed - redirect somewhere (profile?)')}>
+            <Image
+                source={{ uri: 'https://picsum.photos/id/237/200/300' } /* Replace image with logo later */}
+                style={styles.logo}
+            />
+          </TouchableHighlight>
+      </View>
+      <View style={styles.body}>
+
+      </View>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+  },
+  body: {
+    flex: 4,
+    backgroundColor: 'tan',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: deviceHeight / 20,
+    marginLeft: deviceWidth / 20,
+  },
+  logo: {
+    height: deviceWidth / 6,
+    width: deviceWidth / 6,
+  },
+});
