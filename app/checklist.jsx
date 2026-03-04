741d57fe4c179182fabdabbfda74bdfa7f946054
TheBawawa <ap10332n@pace.edu>
2026-03-04 11:58:28 -0500

    UI and alerts woking

--- app/checklist.jsx
+++ app/checklist.jsx
@@ -1,18 +1,23 @@
-import { StyleSheet, Text, View, Dimensions, TouchableHighlight, Image } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View, Dimensions, TouchableHighlight, Image, TextInput} from 'react-native'
 import {Link, Stack} from 'expo-router'
-import Checkbox from 'expo-checkbox'  
-import { ImageBackground } from 'react-native-web';
+import Checkbox from 'expo-checkbox'
+import React, { Component } from 'react';  
 
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
 
-export default function checklist(){
-  return (
+export default class checklist extends Component{
+  state = {
+    task: 'Add Task',
+    task2: 'Add Task'
+  }
+
+  render() {
+    return(
     <View style={styles.container}>
 
       <View style={styles.header}>
-        <Stack/>
+        <Text style={styles.titleP}>Habit Tracker</Text> 
         <TouchableHighlight onPress={() => alert('Logo pressed - redirect to profile')}>
           <Image
             source={{ uri: 'https://picsum.photos/id/237/200/300' } /* Replace image with logo later */}
@@ -29,17 +34,102 @@ export default function checklist(){
           </Text>
         </View>
 
-        <View style={styles.box1}>
+        <View style={styles.buttons}>
+          <View style={styles.icon}>
+            <TouchableHighlight onPress={() => alert('Edit mode pressed')}>
+              <Image source={{ uri: 'https://www.creativefabrica.com/wp-content/uploads/2019/12/16/Black-thin-line-pen-icon-Graphics-1-1-580x386.jpg' }}
+                style={styles.iconImage}/>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.icon}>
+            <TouchableHighlight onPress={() => alert('Delete mode pressed')}>
+              <Image source={{ uri: 'https://i.fbcd.co/products/original/de18ae7d25cea00a569f391100ae56d990105791a99a2d42f35d84477a869d68.jpg' }}
+                style={styles.iconImage}/>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.icon}>
+            <TouchableHighlight onPress={() => alert('Add mode pressed')}>
+              <Image source={{ uri: 'https://static.vecteezy.com/system/resources/thumbnails/000/376/259/small/Basic_Elements__28121_29.jpg' } }
+                style={styles.iconImage}/>
+            </TouchableHighlight>
+          </View>
+        </View>
 
+        <View style={styles.box1}>
+          <View style={styles.box1Title}>
+            <Text style={styles.boxTitleText}>
+              Home
+            </Text>
+            <TextInput 
+              style={styles.taskInput}
+              onChangeText = {(task) => this.setState({task})}
+              value = {this.state.task}
+            />
+            <View style={styles.icon}>
+              <TouchableHighlight onPress={() => alert('Task: "' + this.state.task + '" added!')}>
+                <Image source={{ uri: 'https://static.vecteezy.com/system/resources/thumbnails/000/376/259/small/Basic_Elements__28121_29.jpg' } }
+                  style={styles.boxImage}/>
+              </TouchableHighlight>
+            </View>
+          </View>
+
+          <View style={styles.listTasks}>
+            <View style={styles.task}>
+              <Checkbox style={styles.checkbox}/>
+              <Text style={styles.taskText}>Do dishes </Text>
+            </View>
+            <View style={styles.task}>
+              <Checkbox style={styles.checkbox}/>
+              <Text style={styles.taskText}>Laundry </Text>
+            </View>
+            <View style={styles.task}>
+              <Checkbox style={styles.checkbox}/>
+              <Text style={styles.taskText}>Grocery Shopping </Text>
+            </View>
+          </View>
+          
         </View>
 
         <View style={styles.box2}>
+
+          <View style={styles.box2Title}>
+            <Text style={styles.boxTitleText}>
+              School
+            </Text>
+            <TextInput 
+              style={styles.taskInput}
+              onChangeText = {(task2) => this.setState({task2})}
+              value = {this.state.task2}
+            />
+            <View style={styles.icon}>
+              <TouchableHighlight onPress={() => alert('Task: "' + this.state.task2 + '" added!')}>
+                <Image source={{ uri: 'https://static.vecteezy.com/system/resources/thumbnails/000/376/259/small/Basic_Elements__28121_29.jpg' } }
+                  style={styles.boxImage}/>
+              </TouchableHighlight>
+            </View>
+          </View>
+
+          <View style={styles.listTasks}>
+            <View style={styles.task}>
+              <Checkbox style={styles.checkbox}/>
+              <Text style={styles.taskText}>Do dishes </Text>
+            </View>
+            <View style={styles.task}>
+              <Checkbox style={styles.checkbox}/>
+              <Text style={styles.taskText}>Laundry </Text>
+            </View>
+            <View style={styles.task}>
+              <Checkbox style={styles.checkbox}/>
+              <Text style={styles.taskText}>Grocery Shopping </Text>
+            </View>
+          </View>
 
         </View>
       </View>
 
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -54,6 +144,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  titleP: {
+    fontWeight: 'bold',
+    fontSize: deviceHeight / 30,
+    marginLeft: 20,
   },
 
   logo: {
@@ -85,4 +180,98 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 50,
   },
-})
+
+  buttons:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  iconImage:{
+    height: 50,
+    width: 50,
+
+    borderRadius:40,
+    borderWidth: 1.5,
+
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 25,
+  },
+
+//Task boxes
+  box1:{
+    height: deviceHeight/4,
+    width: deviceWidth/3,
+
+    borderWidth: 3,
+    borderColor: 'red',
+    marginBottom: 30,
+  },
+
+  box1Title:{
+    backgroundColor:'red',
+    padding: 15,
+    flexDirection: 'row',
+  },
+
+  boxTitleText:{
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  taskInput:{
+    width:200,
+    height: 10,
+    backgroundColor:'white',
+
+    borderWidth: 2.2,
+    borderRadius: 20,
+
+    padding: 15,
+    marginLeft: 180,
+  },
+
+  boxImage:{
+    height: 35,
+    width: 35,
+
+    borderRadius:40,
+    borderWidth: 1.5,
+
+    marginLeft: 5,
+    marginRight: 10,
+  },
+
+  box2:{
+    height: deviceHeight/4,
+    width: deviceWidth/3,
+
+    borderWidth: 3,
+    borderColor: 'green',
+  },
+
+  box2Title:{
+    backgroundColor:'green',
+    padding: 15,
+    flexDirection: 'row',
+  },
+
+  //Boxes list of tasks
+  task:{
+    marginLeft: 15,
+    marginTop: 10,
+    flexDirection:'row',
+    alignItems: 'center',
+  },
+
+  checkbox:{
+    height: 20,
+    width: 20,
+    marginRight: 10,
+  },
+
+  taskText:{
+    fontSize: 18,
+  },
+});
