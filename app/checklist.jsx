9bbe1155a71da526f287ff766dc8fe5448f25a12
TheBawawa <ap10332n@pace.edu>
2026-03-04 10:21:51 -0500

    Header Styling

--- app/checklist.jsx
+++ app/checklist.jsx
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View, Dimensions, TouchableHighlight, Image } from 'react-native'
 import React, { useState } from 'react'
-import {Link} from 'expo-router'
-import Checkbox from 'expo-checkbox'
+import {Link, Stack} from 'expo-router'
+import Checkbox from 'expo-checkbox'  
+import { ImageBackground } from 'react-native-web';
 
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
@@ -11,13 +12,19 @@ export default function checklist(){
     <View style={styles.container}>
 
       <View style={styles.header}>
-
+        <Stack/>
+        <TouchableHighlight onPress={() => alert('Logo pressed - redirect to profile')}>
+          <Image
+            source={{ uri: 'https://picsum.photos/id/237/200/300' } /* Replace image with logo later */}
+          style={styles.logo}
+          />
+        </TouchableHighlight>
       </View>
 
       <View style={styles.body}>
 
         <View style={styles.title}>
-          <Text>
+          <Text style={styles.titleText}>
             All Tasks
           </Text>
         </View>
@@ -40,8 +47,42 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
+  //Header elements
   header: {
-    flex: 5,
-    flexDirection: row,
+    flex: 1.5,
+    backgroundColor: 'teal',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  logo: {
+    height: 80,
+    width: 80,
+    marginLeft: 'auto',
+    marginRight: 20,
+    borderRadius: 40,
+  },
+
+  //Body elements
+  body: {
+    flex: 10,
+    backgroundColor: '#E6E6FA',
+    alignItems: 'center',
+  },
+
+  title:{
+    alignItems: 'center',
+
+    borderWidth: 4,
+    borderStyle: 'double',
+
+    padding: 10,
+    margin: 25,
+  },
+
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 50,
   },
 })
