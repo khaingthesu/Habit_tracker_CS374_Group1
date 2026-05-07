bd392cfc2b6fe952519deef7c03dc63f5d2c5104
Khaing The Su <ks78979n@pace.edu>
2026-05-06 21:38:28 -0400

    Fix headerTintColor and Stack.Screen options

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,11 +5,11 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{
         headerStyle: { backgroundColor: '#ece8e8' },
-        headerTintColor: '#dca5a5',
+        headerTintColor: '#1e1c1c',
     }}>
 
       {/* Individual Screens */}
-        <Stack.Screen name="(auth)" option= {{headershown: false}}/>
+        <Stack.Screen name="(auth)"options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ title: "Log In/Sign Up " }} />
         <Stack.Screen name="home" options={{ title: "HomePage" }} />
         <Stack.Screen name="checklist" options={{ title: "Checklist"}} />
